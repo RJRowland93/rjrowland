8de1c8b1c28e0151985cd444061acc54fadda2e5
Randal Rowland <r.rowland93@gmail.com>
2020-10-07 13:58:21 -0500

    sticky footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import { Container } from "theme-ui"
+import { Container, Flex } from "theme-ui"
 
 import Header from "./header"
 import Footer from "./footer"
@@ -27,21 +27,33 @@ const Layout = ({ children }) => {
 
   const [isOpen, setOpen] = useState(false)
   return (
-    <>
+    <Flex
+      sx={{
+        flexDirection: "column",
+        minHeight: "100vh",
+      }}
+    >
       <Header
         siteTitle={data.site.siteMetadata?.title}
         menuLinks={data.site.siteMetadata?.menuLinks}
         isOpen={isOpen}
         setOpen={setOpen}
       />
-      <Container sx={{ minHeight: "100%", opacity: isOpen ? "0.1" : "1" }}>
+
+      <Container
+        as="main"
+        sx={{
+          flex: "1",
+          opacity: isOpen ? "0.1" : "1",
+        }}
+      >
         {children}
       </Container>
       <Footer
         social={data.site.siteMetadata?.socialLinks}
         menuLinks={data.site.siteMetadata?.menuLinks}
       />
-    </>
+    </Flex>
   )
 }
 
